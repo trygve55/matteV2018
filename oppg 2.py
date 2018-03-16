465e1bbfeacee16918c325ea9529ebd56f82dfb5
Trygve Nerland <trygvne@stud.ntnu.no>
2018-03-16 13:00:24 +0100

    renamed function

--- oppg 2.py
+++ oppg 2.py
@@ -3,7 +3,7 @@ from scipy.sparse import spdiags
 from scipy.sparse import lil_matrix
 from scipy.sparse import csr_matrix
 
-def lagA( n ):
+def makeStructureMatrix( n ):
     e = sp.ones(n)
     A = spdiags([e, -4*e, 6*e,-4*e, e],[-2,-1,0,1,2],n,n)
     A = lil_matrix(A)
@@ -16,4 +16,4 @@ def lagA( n ):
     A[n-1, n-4:n] = B[2, :]
     return A
 
-print(lagA(10))
+print(makeStructureMatrix(10))

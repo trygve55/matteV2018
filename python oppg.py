96fa1ae67212f30c9d3f30db43a937f02963f6a3
Trygve Nerland <trygvne@stud.ntnu.no>
2018-03-19 13:01:35 +0100

    grthe

--- python oppg.py
+++ python oppg.py
@@ -3,6 +3,9 @@ from scipy.sparse import spdiags
 from scipy.sparse import lil_matrix
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import spsolve
+import numpy as np
+from numpy import linalg
+import math
 
 g = -9.81 #gravity constant earth (negative)
 L = 2.0 #length
@@ -13,6 +16,7 @@ E = 1.3*10.0**10 #Young's modulus
 I = (w*d**3.0)/12.0 #inertia around the center of mass
 m = density*w*d #mass per meter of beam
 f = m*g #downward force per meter of beam
+p = 100.0 #kg/m
 
 def makeStructureMatrix( n ):
     e = sp.ones(n)
@@ -34,25 +38,59 @@ def getB(n):
 
     return b
 
+def getBextraWeight(n):
+    h = L/n
+
+    b = sp.array([(h**4/(E*I))*f] * n)
+
+    for x in range(n):
+        b[x] += sinusformethaug(x*h, h)
+
+    return b
+
 def getY(x):
     return (f/(24*E*I))*x**2*(x**2 - 4*L*x + 6 * L**2)
 
-def oppg5():
+def getYextraWeight(x):
+    return (f/(24*E*I))*x**2*(x**2 - 4*L*x + 6 * L**2) #- ((g*p*l)/(E*I*math.pi))*((L**3/math.pi**3)*math.sin(math.pi*x/L) - x**3/6 + L*x**2/2) - L**2*x/math.pi**2)
+
+def oppg5(i):
     out = []
     
-    for i in range(10):
+    for i in range(i - 1):
         n = 20 * 2**i
         
         A = makeStructureMatrix(n)
         b = getB(n)
-        #print(A.toarray())
-        #print(b)
-        #b =
         
         y = getY(L) - spsolve(A, b)[-1]
-        out.append({"n" : n, "y" : y})
+        out.append({"n" : n,"kondis" : kondisjonstall(A.toarray()), "y" : y})
 
     return out
+
+def oppg6b(i):
+    out = []
+    
+    for i in range(i - 1):
+        n = 20 * 2**i
+        
+        A = makeStructureMatrix(n)
+        b = getBextraWeight(n)
+        print(spsolve(A, b)[-1])
+        print(b)
+        print(getYextraWeight(n))
+        y = getYextraWeight(L) - spsolve(A, b)[-1]
+        out.append({"n" : n,"kondis" : kondisjonstall(A.toarray()), "y" : y})
+
+    return out
+
+
+def kondisjonstall(A):
+    invers=linalg.inv(A)
+    return linalg.norm(A,np.inf)*linalg.norm(invers,np.inf)
+
+def sinusformethaug(x, h):
+    return -h*p*g*math.sin((x + h/2)*math.pi/L)
 
 n = 10
 A = makeStructureMatrix(n)
@@ -66,6 +104,12 @@ y = spsolve(A, b)
 print(y)
 
 print("Oppg. 5")
-print(oppg5())
+for e in oppg5(6):
+    print(e)
 
+print("Oppg. 6b")
+for e in oppg6b(6):
+    print(e)
+
+print("Oppg. 7")
 
